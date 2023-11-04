4e9c6ead75589cac9ca7c5a37d4f7c30e74aae2e
NicoleJackson89 <>
2023-11-04 13:13:09 +0000

    Add: Game logo and intoduction

--- run.py
+++ run.py
@@ -1,2 +1,15 @@
 # Import section
 import random
+
+# Prints the logo & Welcome message 
+print(r"""
+  _    _          _   _  _____ __  __          _   _ 
+ | |  | |   /\   | \ | |/ ____|  \/  |   /\   | \ | |
+ | |__| |  /  \  |  \| | |  __| \  / |  /  \  |  \| |
+ |  __  | / /\ \ | . ` | | |_ | |\/| | / /\ \ | . ` |
+ | |  | |/ ____ \| |\  | |__| | |  | |/ ____ \| |\  |
+ |_|  |_/_/    \_\_| \_|\_____|_|  |_/_/    \_\_| \_|
+""")
+
+print("Welcome to the word guessing game")
+print("It's all about animals and staying ALIVE...")
