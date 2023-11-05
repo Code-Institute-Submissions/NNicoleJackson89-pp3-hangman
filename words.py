e6812a61da31723b6ad02cfe0c905becb18cabaf
NicoleJackson89 <>
2023-11-05 15:35:12 +0000

    Add: Words list to words.py

---
+++ words.py
@@ -0,0 +1,13 @@
+easy_list = [
+    "Wolf", "Deer", "Crab", "Lion", "Duck", "Hare", "Bear", "Goat"
+]
+
+medium_list = [
+    "Moneky", "Wombat", "Badger", "Turtle", "Weasel", "Alpaca", "Puffin",
+    "Gopher"
+]
+
+hard_list = [
+    "Elephant", "Squirrel", "Hedgehog", "Kangaroo", "Mongoose", "Aardvark",
+    "Platypus", "Reindeer"
+]
