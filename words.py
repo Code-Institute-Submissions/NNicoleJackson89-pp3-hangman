81c0dd69805e4c635e722e1a7663a14e39ad3134
NicoleJackson89 <>
2023-11-07 19:32:58 +0000

    Add: Hangman_stage.py and stages

--- words.py
+++ words.py
@@ -1,16 +1,35 @@
-# easy_list = [
-#     "Wolf", "Deer", "Crab", "Lion", "Duck", "Hare", "Bear", "Goat"
-# ]
+easy_list = [
+    "Wolf",
+    "Deer",
+    "Crab",
+    "Lion",
+    "Duck",
+    "Hare",
+    "Bear",
+    "Goat"
+]
 
-# medium_list = [
-#     "Moneky", "Wombat", "Badger", "Turtle", "Weasel", "Alpaca", "Puffin",
-#     "Gopher"
-# ]
+medium_list = [
+    "Moneky",
+    "Wombat",
+    "Badger",
+    "Turtle",
+    "Weasel",
+    "Alpaca",
+    "Puffin",
+    "Gopher"
+]
 
-# hard_list = [
-#     "Elephant", "Squirrel", "Hedgehog", "Kangaroo", "Mongoose", "Aardvark",
-#     "Platypus", "Reindeer"
-# ]
+hard_list = [
+    "Elephant",
+    "Squirrel",
+    "Hedgehog",
+    "Kangaroo",
+    "Mongoose",
+    "Aardvark",
+    "Platypus",
+    "Reindeer"
+]
 
 words_list = [
     "Elephant", "Squirrel", "Hedgehog", "Kangaroo", "Mongoose", "Aardvark",
