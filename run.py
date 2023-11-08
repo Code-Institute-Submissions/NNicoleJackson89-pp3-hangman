e00d29a347f979d0db8ba3ccdce6849d97b1e3e9
NicoleJackson89 <>
2023-11-08 20:32:36 +0000

    Add: Get stages function

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # Import section
 import random
 from words import words_list
+import words
 from hangman_stage import stages
 
 # Prints the logo & Welcome message
@@ -18,44 +19,65 @@ animals and staying ALIVE... \n"""
 print(welcome_str)
 
 
-# def display_rules():
-#     """
-#     This fuction will display the rules if the user inputs y and skips this
-#     section if n is inputted
-#     """
-#     while True:
-#         question = input("Would you like to see the rules, y/n?\n")
-#         if question.lower() == "y":
-#             print(
-#                 "\n*******************************************************\n" +
-#                 "This is a simple word game, quess one letter at a time.\n" +
-#                 "Guess animal names with 3 difficulty levels:\n" +
-#                 "Easy, medium or hard.\n" +
-#                 "If 6 incorrect letters are guessed, you will be hung.\n" +
-#                 "To win you need to complete the word before being hung.\n" +
-#                 "*******************************************************"
-#                 )
-#             break
-#         elif question.lower() == "n":
-#             break
-#         else:
-#             print("Please enter 'y' for yes or 'n' for no. \n")
+def display_rules():
+    """
+    This fuction will display the rules if the user inputs y and skips this
+    section if n is inputted
+    """
+    while True:
+        question = input("Would you like to see the rules, y/n?\n")
+        if question.lower() == "y":
+            print(
+                "\n*******************************************************\n" +
+                "This is a simple word game, quess one letter at a time.\n" +
+                "Guess animal names with 3 difficulty levels:\n" +
+                "Easy, medium or hard.\n" +
+                "If 6 incorrect letters are guessed, you will be hung.\n" +
+                "To win you need to complete the word before being hung.\n" +
+                "*******************************************************"
+                )
+            break
+        elif question.lower() == "n":
+            break
+        else:
+            print("Please enter 'y' for yes or 'n' for no. \n")
 
 
+def users_name():
+    """
+    This function will ask the user to enter their name, an error will be
+    raised where numbers, characters or spaces are entered
+    """
+    while True:
+        try:
+            name = input("\nPlease enter your name:\n")
+            if not name.isalpha():
+                raise ValueError("Your name can only contain letters.\n")
+            else:
+                print(f"\nReady to play {name.capitalize()}...")
+                print("Good luck!\n")
+                break
+        except ValueError as err:
+            print(err)
 
-# def users_name():
+
+# Need to get this fuction to work with the randowm words befor commiting this function
+# def difficulty_level():
 #     """
-#     This function will ask the user to enter their, an error will be 
-#     raised where numbers, characters or spaces are entered
+#     This function allows the user to select between three levels of
+#     difficulty, easy, medium or hard
 #     """
 #     while True:
 #         try:
-#             name = input("\nPlease enter your name:\n")
-#             if not name.isalpha():
-#                 raise ValueError("Your name can only contain letters.\n")
+#             level = input("Choose easy, medium or hard words?\n").lower()
+#             if level not in ["easy", "medium", "hard"]:
+#                 raise ValueError("You need to type: easy, medium or hard.\n")
+#             elif level == "easy":
+#                 return words.easy_list
+#             elif level == "medium":
+#                 return words.medium_list
 #             else:
-#                 print(f"\nReady to play {name.capitalize()}...\n")
-#                 break
+#                 return words.hard_list
 #         except ValueError as err:
 #             print(err)
 
@@ -69,10 +91,24 @@ def random_word():
     return word.lower()
 
 
-# def get_stages():
-#     max_attempts = 6
-#     return stages
-# print(get_stages(max_attempts))
+def get_stages():
+    """
+    This function prints the hangmans stage and requests the user to input
+    1 letter, if anything else is entered an error will be raised
+    """
+    print(stages[0])
+
+    while True:
+        try:
+            guess = input("Please guess a letter: ").lower()
+            if not guess.isalpha():
+                print("Your guess needs to be an alphabetical letter\n")
+            elif len(guess) > 1:
+                print("Your guess can only be one letter\n")
+            else:
+                return guess
+        except ValueError as err:
+            print(err)
 
 
 
@@ -81,9 +117,17 @@ def random_word():
 
 
 
-# display_rules()
-# users_name()
+
+
+
+
+
+
+display_rules()
+users_name()
+# print(difficulty_level())
 print(random_word())
+get_stages()
 
 
 
@@ -95,28 +139,9 @@ print(random_word())
 
 
 
-# def difficulty_level(words):
-#     """
-#     This function allows the user to select between three levels of
-#     difficulty, easy, medium or hard
-#     """
-#     while True:
-#         try:
-#             print()
-#             level = input("Would you like to play easy, medium or hard?\n").lower()
-#             if level not in ["easy", "medium", "hard"]:
-#                 raise ValueError("You need to choose easy, medium or hard")
-#                 break
-#             elif level == "easy":
-#                 return words.easy_list
-#             elif level == "medium":
-#                 return words.medium_list
-#             else:
-#                 return words.hard_list
-#         except ValueError as err:
-#             print(err)
 
-# difficulty_level()
+
+
 
 
 
